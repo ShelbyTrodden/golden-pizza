465e23565ff3e7b3cc99a706dbcac673010b73b5
Shelby Trodden <shelby.trodden@gmail.com>
2022-07-17 00:08:12 -0500

    WIP working on test 3 researching

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Expected Output: Pizza {size: "medium", toppings: "jalapenos"}
 Test: It should return 3 properties for the Pizza object, size, toppings, and price
 Code userPizza = new Pizza("large",["jalapenos", "pepperoni", "mushrooms"])
 Expected output: Pizza {size: "large", toppings: ["jalapenos", "pepperoni", "mushrooms"] price:0}
+
+
+Describe sizeCost()
+
+Test: It should return the cost of the pizza based off of the crust size
+Code: const pizzaOrder = new Pizza("medium", "jalapeno")
+Expected Output: Pizza {size: "medium", toppings: "jalapenos", price: 1200}
--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,33 @@ function Pizza(size, toppings) {
     this.price = 0;
 }
 
+let pizzaOrder = new Pizza()
+
+Pizza.prototype.sizeCost = function() {
+    if (pizzaOrder.size === "small") {
+        this.price += 1000.00;
+    } else if (pizzaOrder.size === "medium") {
+        this.price += 1200.00;
+    } else if (pizzaOrder.size === "large") {
+        this.price += 1400.00;
+    }    
+    return pizzaOrder.price;
+}
+
 
 //User Interface Logic
+
+window.addEventListener("load", function(){
+    const form = document.getElementById("pizza")
+    const submitButton = document.getElementById("price-btn")
+    const resetBtn = document.getElementById("reset")
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        const result = pizzaOrder.price;
+        (document.getElementById("price").innerText = result);
+
+    })
+
+})
